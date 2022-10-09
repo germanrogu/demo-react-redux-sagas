d7e2157a6827697852d0fba449601c2fb11cb68f
German <german.rodriguezgu@gmail.com>
2022-10-08 20:01:25 -0500

    :zap: counter use selectro useDispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,24 @@
 import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 import { Button, Col, Row, Card } from "antd";
-import { useSelector, useDispatch } from "react-redux";
-import { actionTypes } from "../redux";
+import { useDispatch, useSelector } from "react-redux";
+// import { bindActionCreators } from "redux";
+import { actionCreators, selectorCounter } from "../features/counter";
 
 export const Counter = () => {
-  const counter = useSelector((state) => state.counter);
+  // console.log(useSelector((state) => selectorCounter(state)));
   const dispatch = useDispatch();
+  const { counter } = useSelector((state) => selectorCounter(state));
+
+  // const { counter } = useSelector((state) => state.counter);
+  // console.log(counter.counter);
+
   const increment = () => {
-    dispatch({ type: actionTypes.INCREMENT });
+    dispatch(actionCreators.increment());
   };
   const decrement = () => {
-    dispatch({ type: actionTypes.DECREMENT });
+    dispatch(actionCreators.decrement());
   };
+
   return (
     <>
       <Card title="Counter Redux:" bordered={true} hoverable={true}>
@@ -27,9 +34,9 @@ export const Counter = () => {
           </Col>
           <Col md={4}>
             <span
-              style={{ fontSize: "2rem", fontWeight: "bold", padding: "4rem" }}
+              style={{ fontSize: "2rem", fontWeight: "bold", padding: "2rem" }}
             >
-              {counter}
+              {counter.counter}
             </span>
           </Col>
           <Col md={2}>
@@ -46,3 +53,13 @@ export const Counter = () => {
     </>
   );
 };
+
+// const mapStateToProps = (state) => ({
+//   ...selectorCounter(state),
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   actionCreators: bindActionCreators(actionCreators, dispatch),
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
